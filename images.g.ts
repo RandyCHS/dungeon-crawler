6533fd9861322e892b0e6dc71e8adb66448b91a7
Randy Macdonald <41399033+RandyCHS@users.noreply.github.com>
2026-02-28 14:53:08 -0800

    Just some autogenerated changes to images.g.jres

--- images.g.ts
+++ images.g.ts
@@ -1190,5 +1190,12 @@ f 4 b b b b b b b b b b b b 4 f
         return null;
     })
 
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
